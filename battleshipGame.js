90f5d4f44954d1d16e5b1ee729890b3dd0213492
beckyalter <99490431+beckyalter@users.noreply.github.com>
2022-05-06 10:06:18 -0400

    Setting up object
    Object = model
    ships property
    fire function (initial set up)

--- battleshipGame.js
+++ battleshipGame.js
@@ -23,3 +23,19 @@ view.displayMiss("25");
 view.displayHit("26");
 
 view.displayMessage("Tap tap, is this thing on?");
+
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipsSunk: 0,
+    shipLength: 3,
+    ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+            { locations: ["24", "34", "44"], hits: ["", "", ""] },
+            { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+    
+    fire: function(guess) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+        }
+    }
+}
