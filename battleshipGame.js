4298265f00d81fa5760afb27ab3dd9e12a9122be
beckyalter <99490431+beckyalter@users.noreply.github.com>
2022-05-06 10:29:00 -0400

    Added in view.displayHit, displayMessage, and displayHit

--- battleshipGame.js
+++ battleshipGame.js
@@ -32,9 +32,9 @@ var model = {
     numShips: 3,
     shipsSunk: 0,
     shipLength: 3,
-    ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
-            { locations: ["24", "34", "44"], hits: ["", "", ""] },
-            { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+    ships: [ { locations: ["06", "16", "26"], hits: ["", "", ""] },
+             { locations: ["24", "34", "44"], hits: ["", "", ""] },
+             { locations: ["10", "11", "12"], hits: ["", "", ""] } ],
     
     fire: function(guess) {
         for (var i = 0; i < this.numShips; i++) {
@@ -42,12 +42,17 @@ var model = {
             var index = ship.locations.indexOf(guess);
             if (index >= 0) {
                 ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("Hit!");
                 if (this.isSunk(ship)) {
+                    view.displayMessage("You sank my battleship!");
                     this.shipsSunk++;
                 }
                 return true;
             }
         }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
         return false;
     },
     isSunk: function(ship) {
