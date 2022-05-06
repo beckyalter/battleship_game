9f3214eca7a5439149ae2ea5084beb1d999d31d4
beckyalter <99490431+beckyalter@users.noreply.github.com>
2022-05-06 10:35:26 -0400

    Testing the hardcoded ships
    calling the model's fire method, passing in a row and column each time to be sure we get hit, miss, and "You sank my battleship".

--- battleshipGame.js
+++ battleshipGame.js
@@ -15,16 +15,6 @@ var view = {
     }
 };
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-
-view.displayMessage("Tap tap, is this thing on?");
-
-
 /*Setting up the Object (model): maintains the "state" of the game, with properties: ships (coordinates for each ship), and fire function*/
 
 var model = {
@@ -64,3 +54,17 @@ var model = {
         return true;
     }
 };
+
+model.fire("53");
+
+model.fire("06");
+model.fire("16");
+model.fire("26");
+
+model.fire("34");
+model.fire("24");
+model.fire("44");
+
+model.fire("12");
+model.fire("11");
+model.fire("10");
