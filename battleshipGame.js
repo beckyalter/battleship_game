409d065e98b3bf9952eeed70de376df4165367b0
beckyalter <99490431+beckyalter@users.noreply.github.com>
2022-05-10 15:36:49 -0400

    final code: remove ships, call init function
    Removed hardcoded ships, added empty arrays for new ships, and called the model.generateShipLocations from the init function so we have ships in locations right away and can start playing the game!

--- battleshipGame.js
+++ battleshipGame.js
@@ -15,31 +15,7 @@ var view = {
     }
 };
 
-/*Setting the event handler to the Fire! button*/
 
-function init() {
-    var fireButton = document.getElementById("fireButton");
-    fireButton.onclick = handleFireButton;
-    var guessInput = document.getElementById("guessInput");
-    guessInput.onkeypress = handleKeyPress;
-}
-
-function handleKeyPress(e) {
-    var fireButton = document.getElementById("fireButton");
-    if (e.keyCode === 13) {
-        fireButton.click();
-        return false;
-    }
-}
-
-function handleFireButton() {
-    var guessInput = document.getElementById("guessInput");
-    var guess = guessInput.value;
-    controller.processGuess(guess);
-    guessInput.value= "";
-}
-
-window.onload = init;
 
 /*Setting up the Object (model): maintains the "state" of the game, with properties: ships (coordinates for each ship), and fire function*/
 
@@ -48,9 +24,14 @@ var model = {
     numShips: 3,
     shipsSunk: 0,
     shipLength: 3,
+    /*removing hardcoded ship locations and adding new empty arrays beneathe
     ships: [ { locations: ["06", "16", "26"], hits: ["", "", ""] },
              { locations: ["24", "34", "44"], hits: ["", "", ""] },
              { locations: ["10", "11", "12"], hits: ["", "", ""] } ],
+    */
+    ships: [ { locations: [0, 0, 0], hits: ["", "", ""] },
+             { locations: [0, 0, 0], hits: ["", "", ""] },
+             { locations: [0, 0, 0], hits: ["", "", ""] } ],
     
     fire: function(guess) {
         for (var i = 0; i < this.numShips; i++) {
@@ -78,8 +59,85 @@ var model = {
             }
         }
         return true;
+    },
+    
+    /*removing hardcoded ship locations and creating random ship placement*/
+    
+    generateShipLocations: function() {
+        var locations;
+        for (var i = 0; i <this.numShips; i++) {
+            do {
+                locations = this.generateShip();
+            } while (this.collision(locations));
+            this.ships[i].locations = locations;
+        }
+    },
+    generateShip: function() {
+        var direction = Math.floor(Math.random() * 2);
+        var row;
+        var col;
+        if (direction === 1) {
+            //Generate a starting location for a horizontal ship
+            row = Math.floor(Math.random() * this.boardSize);
+            col = Math.floor(Math.random() * (this.boardSize - (this.shipLength + 1)));
+        } else {
+            //Generate a starting location for a vertical ship
+            row = Math.floor(Math.random() * (this.boardSize - (this.shipLength + 1)));
+            col = Math.floor(Math.random() * this.boardSize);
+        }
+
+        var newShipLocations = [];
+        for (var i = 0; i < this.shipLength; i++) {
+            if (direction === 1) {
+                //Add location to array for new horizontal ship
+                newShipLocations.push(row + "" + (col + i));
+            } else {
+                //Add location to array for a new vertical ship
+                newShipLocations.push((row + i) + "" + col);
+            }
+        }
+        return newShipLocations;
+    }, 
+    collision: function(locations) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            for (var j = 0; j < locations.length; j++) {
+                if (ship.locations.indexOf(locations[j]) >= 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 };
+
+/*Setting the event handler to the Fire! button*/
+
+function init() {
+    var fireButton = document.getElementById("fireButton");
+    fireButton.onclick = handleFireButton;
+    var guessInput = document.getElementById("guessInput");
+    guessInput.onkeypress = handleKeyPress;
+
+    model.generateShipLocations();
+}
+
+function handleKeyPress(e) {
+    var fireButton = document.getElementById("fireButton");
+    if (e.keyCode === 13) {
+        fireButton.click();
+        return false;
+    }
+}
+
+function handleFireButton() {
+    var guessInput = document.getElementById("guessInput");
+    var guess = guessInput.value;
+    controller.processGuess(guess);
+    guessInput.value= "";
+}
+
+window.onload = init;
 
 /*Setting up the Controller */
 
