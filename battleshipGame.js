f2dc24294fb19b23b1d6c23569fa9807d51c3724
beckyalter <99490431+beckyalter@users.noreply.github.com>
2022-05-10 14:10:42 -0400

    Added handler for the RETURN key to fire

--- battleshipGame.js
+++ battleshipGame.js
@@ -20,6 +20,16 @@ var view = {
 function init() {
     var fireButton = document.getElementById("fireButton");
     fireButton.onclick = handleFireButton;
+    var guessInput = document.getElementById("guessInput");
+    guessInput.onkeypress = handleKeyPress;
+}
+
+function handleKeyPress(e) {
+    var fireButton = document.getElementById("fireButton");
+    if (e.keyCode === 13) {
+        fireButton.click();
+        return false;
+    }
 }
 
 function handleFireButton() {
